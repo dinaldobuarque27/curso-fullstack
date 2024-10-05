f525a34068fa3249d8bc396f498ad30936928190
Naldinho <dinaldo_buarque@hotmail.com>
2024-10-05 20:10:37 -0300

    resolução do exercicio19 sessão 05

---
+++ exercicio-java/src/exercico19S05.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class exercico19S05 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite seu salario: ");
+        double salario = sc.nextDouble();
+
+        sc.close();
+
+        double aumento = 0;
+
+        if(salario > 0 && salario <=300){
+            aumento = 1.05;
+        } else if (salario > 300 && salario <= 500 ){
+            aumento = 1.07;
+        }else if (salario > 500){
+            aumento = 1.10;
+        } else {
+            System.out.println("não existe salario negativo.");
+        }
+
+        //double novoSalario = salario * aumento;
+
+        System.out.printf("NOVO SALARIO: R$%.2f", salario * aumento);
+    }
+    
+}
